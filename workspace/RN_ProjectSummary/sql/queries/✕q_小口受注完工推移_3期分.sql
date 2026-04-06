d568ca2ddf6e41264fb157bdeacb1218e6e800d1
Ez <1974eiji2@gmail.com>
2026-04-06 09:59:32 +0900

    Auto-sync: RN_ProjectSummary.accdb

--- workspace/RN_ProjectSummary/sql/queries/✕q_小口受注完工推移_3期分.sql
+++
@@ -1,7 +0,0 @@
-SELECT at_Icube_累計.[施工管轄組織名], at_Icube_累計.[受注期] & '期' AS 受注期_, at_Icube_累計.[完工期] & '期' AS 完工期_, at_Icube_累計.[受注月] & '月' AS 受注月_, at_Icube_累計.[完工月] & '月' AS 完工月_, CCur(Nz(Sum(at_Icube_累計.[工事価格]), 0)) AS 工事価格の合計
-FROM at_Icube_累計
-WHERE at_Icube_累計.[施工管轄組織名] IN ('青森ＲＮ（作）', '秋田ＲＮ（作）', '盛岡ＲＮ（作）', '山形ＲＮ（作）', '福島ＲＮ（作）', '仙台ＲＮ（作）')
-    AND at_Icube_累計.[一件工事判定] = '小口工事'
-    AND at_Icube_累計.[受注期] >= (SELECT Max([受注期]) FROM [at_Icube_累計]) - 2
-GROUP BY at_Icube_累計.[施工管轄組織名], at_Icube_累計.[受注期], at_Icube_累計.[完工期], at_Icube_累計.[受注月], at_Icube_累計.[完工月]
-ORDER BY at_Icube_累計.[施工管轄組織名], at_Icube_累計.[受注期], at_Icube_累計.[完工期], at_Icube_累計.[受注月], at_Icube_累計.[完工月];
--- workspace/RN_ProjectSummary/sql/tables/✕at_原価S_ColSetting2.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE [✕at_原価S_ColSetting2] (
-    [ID] LONG,
-    [仮テーブルタイトル名] TEXT,
-    [基本工事_タイトル名] TEXT,
-    [基本工事_データ型] TEXT,
-    [枝番工事_タイトル名] TEXT,
-    [枝番工事_データ型] TEXT,
-    CONSTRAINT PK_✕at_原価S_ColSetting2 PRIMARY KEY ([ID])
-);
